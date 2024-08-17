88d6a967c41d8c078dcb12668fc8d99d0f8ead12
VGD3626 <vrund3626@gmail.com>
2024-08-17 17:00:22 +0530

    On branch vrund Changes to be committed: 	new file:   Blogging_Application/README.md 	new file:   Blogging_Application/home.aspx 	new file:   Blogging_Application/home.aspx.cs 	new file:   Blogging_Application/home.aspx.designer.cs

---
+++ Blogging_Application/home.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="Blogging_Application.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Blog Details</title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style8 {
+            height: 42px;
+            font-size: 22px;
+        }
+        .form-table {
+            width: 100%;
+            margin-top: 20px;
+        }
+        .form-table td {
+            padding: 10px;
+        }
+        .form-table input[type="text"] {
+            width: 300px;
+            padding: 5px;
+        }
+        .auto-style10 {
+            width: 48px;
+            height: 24px;
+        }
+        .auto-style12 {
+            height: 24px;
+        }
+        .auto-style13 {
+            height: 24px;
+            width: 492px;
+        }
+        .auto-style15 {
+            height: 24px;
+            width: 256px;
+        }
+        .auto-style17 {
+            width: 45px;
+            height: 24px;
+        }
+        .custom-style1 {
+            font-family: Bahnschrift;
+        }
+        .auto-style18 {
+            margin-left: 0px;
+        }
+    </style>
+</head>
+<body class="custom-style1">
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style8">
+                    <asp:LinkButton ID="linkbtnhome" runat="server" BackColor="#CCCCFF" BorderStyle="None" Font-Bold="True" Font-Italic="False" Font-Names="Bahnschrift" Font-Size="Medium" Font-Underline="False" ForeColor="#0066FF" Height="38px" OnClick="LinkbtnLogo_Click" OnClientClick="on" Width="705px">Blogging Application</asp:LinkButton>
+                    <asp:LinkButton ID="linkbtnFeed" runat="server" BackColor="#DDDDFF" BorderStyle="None" Font-Bold="False" Font-Italic="False" Font-Names="Bahnschrift" Font-Size="Small" Font-Underline="False" ForeColor="Blue" Height="38px" OnClientClick="on" OnClick="LinkbtnFeed_Click" Width="133px" CssClass="auto-style18">Feed</asp:LinkButton>
+                    <asp:LinkButton ID="linkbtnMyBlogs" runat="server" BackColor="#DDDDFF" BorderStyle="None" Font-Bold="False" Font-Italic="False" Font-Names="Bahnschrift" Font-Size="Small" Font-Underline="False" ForeColor="Blue" Height="38px" OnClientClick="on" OnClick="LinkbtnMyBlogs_Click" Width="133px">My blogs</asp:LinkButton>
+                    <asp:LinkButton ID="linkbtnLogOut" runat="server" BackColor="#DDDDFF" BorderStyle="None" Font-Bold="False" Font-Italic="False" Font-Names="Bahnschrift" Font-Size="Small" Font-Underline="False" ForeColor="Blue" Height="38px" OnClientClick="on" OnClick="LinkbtnLogout_Click" Width="133px">Log out</asp:LinkButton>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+
+        <!-- Blog Details Form -->
+    <table class="auto-style1">
+        <tr>
+            <td>
+        <asp:PlaceHolder ID="BlogPlaceHolder" runat="server"></asp:PlaceHolder>
+            </td>
+        </tr>
+    </table>
+        <p>
+            &nbsp;</p>
+    </form>
+    </body>
+</html>
---
+++ Blogging_Application/home.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Blogging_Application
+{
+    public partial class home : System.Web.UI.Page
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+        private DataSet ds;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadBlogData();
+            }
+        }
+
+        private void LoadBlogData()
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            connection = new SqlConnection(connStr);
+            command = new SqlCommand("SELECT * FROM Blog", connection);
+            adapter = new SqlDataAdapter(command);
+            ds = new DataSet();
+
+            try
+            {
+                connection.Open();
+                adapter.Fill(ds, "Blog");
+
+                foreach (DataRow r in ds.Tables["Blog"].Rows)
+                {
+
+                    Label authorLabel = new Label();
+                    TextBox authorText = new TextBox();
+                    Label titleLabel = new Label();
+                    TextBox titleText = new TextBox();  
+                    Label dateLabel = new Label();
+                    TextBox dateText = new TextBox();
+                    TextBox blogContent = new TextBox();
+
+                    titleLabel.Text = "Title:";
+                    titleLabel.Height = 28;
+                    titleText.Text = r["title"].ToString();
+                    titleText.Height = 28;
+                    titleText.BackColor = System.Drawing.Color.FromArgb(230, 230, 255);
+                    titleText.BorderStyle = BorderStyle.None;
+                    titleText.Enabled = false;
+
+                    authorLabel.Text = "Author:";
+                    authorLabel.Height = 28;
+                    authorText.Text = r["author"].ToString();
+                    authorText.Height = 28;
+                    authorText.BackColor = System.Drawing.Color.FromArgb(230, 230, 255);
+                    authorText.BorderStyle = BorderStyle.None;
+                    authorText.Enabled = false;
+
+                    dateLabel.Text = "Date:";
+                    dateLabel.Height = 28;
+                    dateText.Text = r["date"].ToString();
+                    dateText.Height = 28;
+                    dateText.BackColor = System.Drawing.Color.FromArgb(230, 230, 255);
+                    dateText.BorderStyle = BorderStyle.None;
+                    dateText.Enabled = false;
+
+                    blogContent.TextMode = TextBoxMode.MultiLine;
+                    blogContent.Height = 283;
+                    blogContent.Width = 1069;
+                    blogContent.Text = r["content"].ToString();
+                    blogContent.BackColor = System.Drawing.Color.FromArgb(255, 255, 206);
+                    blogContent.BorderStyle = BorderStyle.None;
+                    blogContent.Enabled = false;
+
+                    PlaceHolder placeHolder = new PlaceHolder();
+                    placeHolder.Controls.Add(titleLabel);
+                    placeHolder.Controls.Add(titleText);
+                    placeHolder.Controls.Add(authorLabel);
+                    placeHolder.Controls.Add(authorText);
+                    placeHolder.Controls.Add(dateLabel);
+                    placeHolder.Controls.Add(dateText);
+                    placeHolder.Controls.Add(new Literal { Text = "<br/>" });
+                    placeHolder.Controls.Add(blogContent);
+                    placeHolder.Controls.Add(new Literal { Text = "<br/><br/><br/>" });
+
+                    BlogPlaceHolder.Controls.Add(placeHolder);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        protected void LinkbtnFeed_Click(object sender, EventArgs e)
+        {
+            Response.Write("<h1>Hey</h1>");
+        }
+
+        protected void LinkbtnLogout_Click(object sender, EventArgs e)
+        {
+            Response.Write("<h1>Hey</h1>");
+        }
+
+        protected void LinkbtnMyBlogs_Click(object sender, EventArgs e)
+        {
+            Response.Write("<h1>Hey</h1>");
+        }
+
+        protected void LinkbtnLogo_Click(object sender, EventArgs e)
+        {
+            Response.Write("<h1>Hey</h1>");
+        }
+    }
+}
